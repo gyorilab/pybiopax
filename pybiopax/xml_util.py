a554e60ef9ff472d3808f81bbfccd7076f0e6133
Ben Gyori <ben.gyori@gmail.com>
2020-05-28 15:15:21 -0400

    Add docstrings to xml utils

--- pybiopax/xml_util.py
+++ pybiopax/xml_util.py
@@ -19,6 +19,7 @@ makers = {
 
 
 def wrap_xml_elements(elements):
+    """Return a valid BioPAX OWL wrapping XML-serialized BioPAX objects."""
     # We first make the RDF wrapper and add an Ontology element first
     rdfm = ElementMaker(namespace=namespaces['rdf'],
                         nsmap=namespaces)
@@ -37,6 +38,7 @@ def wrap_xml_elements(elements):
 
 
 def xml_to_str(xml):
+    """Return the OWL string for an XML element tree."""
     xmlb = etree.tostring(xml, pretty_print=True,
                           encoding='utf-8', xml_declaration=True)
     xmls = xmlb.decode('utf-8')
@@ -47,23 +49,28 @@ def xml_to_str(xml):
 
 
 def xml_to_file(xml, fname):
+    """Write an XML element tree to a given file."""
     with open(fname, 'w') as fh:
         fh.write(xml_to_str(xml))
 
 
 def nselem(ns, elem):
+    """Return a full namespaced string with curly brackets with a suffix."""
     return '{%s}%s' % (namespaces[ns], elem)
 
 
 def nssuffix(ns, suffix):
+    """Return a full namespaced string with a suffix."""
     return '%s%s' % (namespaces[ns], suffix)
 
 
 def get_datatype(attrib):
+    """Return the RDF data type of an element attribute."""
     return attrib.get(nselem('rdf', 'datatype'))
 
 
 def get_resource(attrib):
+    """Return the resource associated with an element attribute."""
     res = attrib.get(nselem('rdf', 'resource'))
     if res:
         if res.startswith('#'):
@@ -73,39 +80,48 @@ def get_resource(attrib):
 
 
 def is_url(txt):
+    """Return true if the given string is an URL."""
     return txt.startswith('http')
 
 
 def is_datatype(attrib, datatype):
+    """Return True of the given attribute is of a given type."""
     return get_datatype(attrib) == datatype
 
 
 def get_tag(element):
+    """Return the tag of an element."""
     return re.match(r'.*}(.+)', element.tag).groups()[0]
 
 
 def get_attr_tag(element):
+    """Return the tag of an element as an attribute name."""
     raw_tag = get_tag(element)
     return camel_to_snake(raw_tag)
 
 
 def get_id_or_about(element):
+    """Return the ID or the about associated with an element"""
     return element.attrib.get(nselem('rdf', 'ID')) or \
-           element.attrib.get(nselem('rdf', 'about'))
+        element.attrib.get(nselem('rdf', 'about'))
 
 
 def get_ns(element):
+    """Return the name space of a given element."""
     return re.match(r'\{(.*)\}', element.tag).groups()[0]
 
 
 def has_ns(element, ns):
+    """Return True if the element is from a given name space."""
     return get_ns(element) == namespaces[ns]
 
 
 def camel_to_snake(txt):
+    """Return snake case from camel case"""
     return re.sub(r'(?<!^)(?=[A-Z])', '_', txt).lower()
 
 
 def snake_to_camel(txt):
+    """Return camel case from snake case."""
     parts = txt.split('_')
     return parts[0] + ''.join([p.capitalize() for p in parts[1:]])
