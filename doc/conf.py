259c36c116b78c401378427164b459c53d803a44
Ben Gyori <ben.gyori@gmail.com>
2020-11-18 21:07:19 -0500

    Bump version to 0.0.5

--- doc/conf.py
+++ doc/conf.py
@@ -24,7 +24,7 @@ author = 'Benjamin M. Gyori'
 # The short X.Y version
 version = '0.0'
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.0.5'
 
 
 # -- General configuration ---------------------------------------------------
