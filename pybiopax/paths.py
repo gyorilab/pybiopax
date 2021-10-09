f2b74a6d889bb53b2e478d942c27af0de8eed157
Ben Gyori <ben.gyori@gmail.com>
2021-10-08 21:16:02 -0400

    Implement class constraints

--- pybiopax/paths.py
+++ pybiopax/paths.py
@@ -1,4 +1,10 @@
-from .biopax import BioPaxObject
+__all__ = ['find_objects', 'BiopaxClassConstraintError']
+
+import logging
+from .biopax import *
+
+
+logger = logging.getLogger(__name__)
 
 
 def find_objects(start_obj: BioPaxObject, path_str: str):
@@ -6,13 +12,22 @@ def find_objects(start_obj: BioPaxObject, path_str: str):
     last = True if len(parts) == 1 else False
     for part in parts:
         if ':' in part:
-            attribute, class_constraint = part.split(':', maxsplit=1)
+            attribute, class_constraint_str = part.split(':', maxsplit=1)
+            try:
+                cls = biopax_cls_map[class_constraint_str]
+            except KeyError as e:
+                raise BiopaxClassConstraintError(f'{class_constraint_str} '
+                                                 f'is not a valid BioPax class '
+                                                 f'name.')
         else:
-            attribute, class_constraint = part, None
+            attribute, cls = part, None
+
         val = getattr(start_obj, attribute, None)
         if val is None:
             return []
         elif isinstance(val, BioPaxObject):
+            if cls and not isinstance(val, cls):
+                return []
             if not last:
                 return find_objects(val, '/'.join(parts[1:]))
             else:
@@ -20,8 +35,24 @@ def find_objects(start_obj: BioPaxObject, path_str: str):
         elif isinstance(val, list):
             results = []
             for v in val:
+                if cls and not isinstance(v, cls):
+                    continue
                 if not last:
                     results.append(find_objects(v, '/'.join(parts[1:])))
                 else:
                     results.append(v)
             return results
+
+
+def _make_biopax_cls_map():
+    import inspect
+    from pybiopax import biopax
+    return {k: v for k, v in inspect.getmembers(biopax, inspect.isclass)
+            if issubclass(v, BioPaxObject)}
+
+
+biopax_cls_map = _make_biopax_cls_map()
+
+
+class BiopaxClassConstraintError(KeyError):
+    pass
--- pybiopax/tests/test_paths.py
+++ pybiopax/tests/test_paths.py
@@ -1,5 +1,5 @@
 from pybiopax.biopax import *
-from pybiopax.paths import find_objects
+from pybiopax.paths import find_objects, BiopaxClassConstraintError
 
 
 def test_find_objects():
@@ -21,3 +21,27 @@ def test_find_objects():
     objects = find_objects(prot, 'entity_reference/entity_reference_of')
     assert len(objects) == 1, objects
     assert objects[0] == prot, objects
+
+
+def test_find_objects_constraints():
+    xr1 = UnificationXref(uid='2')
+    xr2 = PublicationXref(uid='3')
+    xr3 = RelationshipXref(uid='4')
+    protref = EntityReference(uid='1', xref=[xr1, xr2, xr3])
+    xr1._xref_of = [protref]
+    xr2._xref_of = [protref]
+    xr3._xref_of = [protref]
+
+    objects = find_objects(protref, 'xref')
+    assert len(objects) == 3
+
+    objects = find_objects(protref, 'xref:Xref')
+    assert len(objects) == 3
+
+    objects = find_objects(protref, 'xref:UnificationXref')
+    assert len(objects) == 1
+    assert objects[0] == xr1
+
+    objects = find_objects(protref, 'xref:UnificationXref/xref_of')
+    assert len(objects) == 1
+    assert objects[0] == protref, objects
