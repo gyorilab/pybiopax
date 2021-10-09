441a1cc04c3e0b71551bb91f414540805ccfd3bc
Ben Gyori <ben.gyori@gmail.com>
2021-10-09 10:42:21 -0400

    Factor out for loop due to recursion

--- pybiopax/paths.py
+++ pybiopax/paths.py
@@ -32,56 +32,59 @@ def find_objects(start_obj: BioPaxObject, path_str: str) -> List[BioPaxObject]:
     :
         A list of BioPaxObjects satisfying the given path specification.
     """
-    parts = path_str.split('/')
-    last = True if len(parts) == 1 else False
-    for part in parts:
-        if ':' in part:
-            attribute, class_constraint_str = part.split(':', maxsplit=1)
-            try:
-                cls = biopax_cls_map[class_constraint_str]
-            except KeyError:
-                raise BiopaxClassConstraintError(f'{class_constraint_str} '
-                                                 f'is not a valid BioPax class '
-                                                 f'name.') from None
+    # We split off the first part and separate out the rest
+    part, rest = path_str.split('/', maxsplit=1) \
+        if '/' in path_str else (path_str, None)
+
+    # Handle class constraint
+    if ':' in part:
+        attribute, class_constraint_str = part.split(':', maxsplit=1)
+        try:
+            cls = biopax_cls_map[class_constraint_str]
+        except KeyError:
+            raise BiopaxClassConstraintError(f'{class_constraint_str} '
+                                             f'is not a valid BioPax class '
+                                             f'name.') from None
+    else:
+        attribute, cls = part, None
+
+    # Handle recursion marker
+    if attribute.endswith('*'):
+        attribute = attribute[:-1]
+        recursive = True
+    else:
+        recursive = False
+
+    # Get the attribute we are looking for
+    attr_val = getattr(start_obj, attribute, None)
+    # We turn the value into a flat list of BioPaxObjects
+    val = _get_object_list(attr_val)
+
+    # If this is a recursive part, we run a BFS to get all the downstream
+    # objects that can be reached via one or more of the given type of
+    # attribute links
+    if recursive:
+        queue = val[:]
+        visited = val[:]
+        while queue:
+            obj = queue.pop(0)
+            obj_val = getattr(obj, attribute, None)
+            for child in _get_object_list(obj_val):
+                if child not in visited:
+                    visited.append(child)
+                    queue.append(child)
+        val = visited
+
+    # At this point, val is guaranteed to be a list of BioPaxObjects
+    results = []
+    for v in val:
+        if cls and not isinstance(v, cls):
+            continue
+        if rest:
+            results.append(find_objects(v, rest))
         else:
-            attribute, cls = part, None
-
-        if attribute.endswith('*'):
-            attribute = attribute[:-1]
-            recursive = True
-        else:
-            recursive = False
-
-        # Get the attribute we are looking for
-        attr_val = getattr(start_obj, attribute, None)
-        # We turn the value into a flat list of BioPaxObjects
-        val = _get_object_list(attr_val)
-
-        # If this is a recursive part, we run a BFS to get all the downstream
-        # objects that can be reached via one or more of the given type of
-        # attribute links
-        if recursive:
-            queue = val[:]
-            visited = val[:]
-            while queue:
-                obj = queue.pop(0)
-                obj_val = getattr(obj, attribute, None)
-                for child in _get_object_list(obj_val):
-                    if child not in visited:
-                        visited.append(child)
-                        queue.append(child)
-            val = visited
-
-        # At this point, val is guaranteed to be a list of BioPaxObjects
-        results = []
-        for v in val:
-            if cls and not isinstance(v, cls):
-                continue
-            if not last:
-                results.append(find_objects(v, '/'.join(parts[1:])))
-            else:
-                results.append([v])
-        return list(itertools.chain(*results))
+            results.append([v])
+    return list(itertools.chain(*results))
 
 
 def _get_object_list(val):
