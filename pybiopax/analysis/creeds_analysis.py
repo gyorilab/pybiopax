28f0aa8fed54cd7055293053b84f78bfb091bae4
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-03 21:18:22 +0100

    Update creeds_analysis.py

--- pybiopax/analysis/creeds_analysis.py
+++ pybiopax/analysis/creeds_analysis.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import pickle
 from collections import defaultdict
 from typing import Optional, Type
@@ -5,10 +7,11 @@ from typing import Optional, Type
 import bioversions
 import numpy as np
 import pandas as pd
+import seaborn
 from scipy.stats import fisher_exact
 from statsmodels.stats.multitest import multipletests
 from tqdm import tqdm
-from functools import lru_cache
+
 import bioregistry
 import protmapper.uniprot_client
 import pybiopax
@@ -16,7 +19,10 @@ import pyobo
 import pystow
 from indra.sources import creeds
 from indra.statements import (
-    Agent, RegulateAmount, Statement, stmts_to_json_file,
+    Agent,
+    RegulateAmount,
+    Statement,
+    stmts_to_json_file,
 )
 from pybiopax.biopax import Protein
 
@@ -27,25 +33,17 @@ CREEDS_MODULE = pystow.module("bio", "creeds")
 def get_reactome_human_ids() -> set[str]:
     identifiers = pyobo.get_ids("reactome")
     species = pyobo.get_id_species_mapping("reactome")
-    rv = {
-        reactome_id
-        for reactome_id in identifiers
-        if species[reactome_id] == "9606"
-    }
-    return set(sorted(rv)[:5])
+    rv = {reactome_id for reactome_id in identifiers if species[reactome_id] == "9606"}
+    return rv
 
 
 def get_protein_hgnc(protein: Protein) -> Optional[str]:
     if protein.entity_reference is None:
         return None
-    rv = {
-        bioregistry.normalize_prefix(xref.db): xref.id
-        for xref in protein.entity_reference.xref
-    }
+    rv = {bioregistry.normalize_prefix(xref.db): xref.id for xref in protein.entity_reference.xref}
     hgnc_id = rv.get("hgnc")
     if hgnc_id is not None:
         return hgnc_id
-
     uniprot_id = rv.get("uniprot")
     if uniprot_id is not None:
         hgnc_id = protmapper.uniprot_client.get_hgnc_id(uniprot_id)
@@ -62,9 +60,15 @@ def get_protein_hgnc(protein: Protein) -> Optional[str]:
 def ensure_reactome(reactome_id: str, force: bool = False):
     path = REACTOME_MODULE.join(name=f"{reactome_id}.xml")
     if path.is_file() and not force:
-        return pybiopax.model_from_owl_file(path)
-    model = pybiopax.model_from_reactome(reactome_id)
-    pybiopax.model_to_owl_file(model, path)
+        return pybiopax.model_from_owl_file(
+            path, tqdm_kwargs=dict(leave=False, desc=f"Parsing {reactome_id} OWL")
+        )
+    model = pybiopax.model_from_reactome(
+        reactome_id, tqdm_kwargs=dict(leave=False, desc=f"Getting {reactome_id}")
+    )
+    pybiopax.model_to_owl_file(
+        model, path, tqdm_kwargs=dict(leave=False, desc=f"Serializing {reactome_id} OWL")
+    )
     return model
 
 
@@ -82,13 +86,13 @@ def get_reactome_genes(reactome_id: str) -> set[str]:
 def get_creeds_statements(entity_type: str) -> list[Statement]:
     path = CREEDS_MODULE.join(name=f"{entity_type}_stmts.pkl")
     if path.is_file():
-        with path.open('rb') as file:
+        with path.open("rb") as file:
             return pickle.load(file)
     url = creeds.api.urls[entity_type]
     raw_path = CREEDS_MODULE.ensure(url=url)
     processor = creeds.process_from_file(raw_path, entity_type)
     stmts_to_json_file(processor.statements, path)
-    with path.open('wb') as file:
+    with path.open("wb") as file:
         pickle.dump(processor.statements, file, protocol=pickle.HIGHEST_PROTOCOL)
     return processor.statements
 
@@ -99,7 +103,7 @@ def get_hgnc_id(agent: Agent) -> Optional[str]:
         return hgnc_id
     up_id = agent.db_refs.get("UP")
     if up_id is None:
-        return
+        return None
     return protmapper.uniprot_client.get_hgnc_id(up_id)
 
 
@@ -131,47 +135,55 @@ def _prepare_hypergeometric_test(
     :param gene_universe: number of HGNC symbols
     :return: 2x2 matrix
     """
-    return np.array([
+    return np.array(
         [
-            len(query_gene_set.intersection(pathway_gene_set)),
-            len(query_gene_set.difference(pathway_gene_set)),
-        ],
-        [
-            len(pathway_gene_set.difference(query_gene_set)),
-            gene_universe - len(pathway_gene_set.union(query_gene_set)),
-        ],
-    ])
+            [
+                len(query_gene_set.intersection(pathway_gene_set)),
+                len(query_gene_set.difference(pathway_gene_set)),
+            ],
+            [
+                len(pathway_gene_set.difference(query_gene_set)),
+                gene_universe - len(pathway_gene_set.union(query_gene_set)),
+            ],
+        ]
+    )
 
 
 def _main():
-    universe_size = len(pyobo.get_ids("hgnc"))
-    creeds_drug_stmts = get_creeds_statements("gene")
-    creeds_drug_hgncs = get_regulates(creeds_drug_stmts)
-
     reactome_ids = get_reactome_human_ids()
     reactome_it = [
         (reactome_id, get_reactome_genes(reactome_id))
-        for reactome_id in reactome_ids
+        for reactome_id in tqdm(reactome_ids, desc="Downloading Reactome pathways")
     ]
+
+    universe_size = len(pyobo.get_ids("hgnc"))
+    creeds_drug_stmts = get_creeds_statements("gene")
+    creeds_drug_hgncs = get_regulates(creeds_drug_stmts)
 
     dfs = []
     for pert_id, pert_genes in tqdm(creeds_drug_hgncs.items()):
         rows = []
         for reactome_id, reactome_genes in tqdm(reactome_it, leave=False):
             table = _prepare_hypergeometric_test(pert_genes, reactome_genes, universe_size)
-            _, p_value = fisher_exact(table, alternative='greater')
-            rows.append((f'hgnc:{pert_id}', reactome_id, p_value))
-        df = pd.DataFrame(rows, columns=['perturbation', 'pathway', 'p'])
-        correction_test = multipletests(df["p"], method='fdr_bh')
-        df['q'] = correction_test[1]
+            _, p_value = fisher_exact(table, alternative="greater")
+            rows.append((f"hgnc:{pert_id}", f"reactome:{reactome_id}", p_value))
+        df = pd.DataFrame(rows, columns=["perturbation", "pathway", "p"])
+        correction_test = multipletests(df["p"], method="fdr_bh")
+        df["q"] = correction_test[1]
+        df["mlq"] = -np.log10(df["q"])  # minus log q
         df.sort_values("q", inplace=True)
         dfs.append(df)
 
     path = CREEDS_MODULE.join(name="analysis.tsv")
     full_df = pd.concat(dfs)
-    full_df.to_csv(path, sep='\t', index=False)
+    full_df.to_csv(path, sep="\t", index=False)
     print("output to", path)
 
+    df_square = full_df.pivot(index="pathway", columns="perturbation")["mlq"]
+    img_path = CREEDS_MODULE.join(name="analysis.tsv")
+    g = seaborn.clustermap(df_square)
+    g.savefig(img_path)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     _main()
