c477863d090dbf2bff5b445978793e3946d3de54
Ben Gyori <ben.gyori@gmail.com>
2021-12-14 09:56:35 -0500

    Use tests_require in setup

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(name='pybiopax',
       ],
       packages=find_packages(),
       install_requires=['lxml', 'requests', 'tqdm'],
-      extras_require={'test': ['tox']},
+      tests_require=['pytest', 'tox'],
       keywords=['biology', 'pathway']
       )
